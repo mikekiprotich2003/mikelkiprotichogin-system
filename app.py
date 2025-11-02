7c73f9128fedb7965575e344a491d209c73854f3
ron-kiplimo <rkkiplimo@gmail.com>
2025-11-02 22:49:51 +0300

    Fix: bind to 0.0.0.0 and use PORT

--- app.py
+++ app.py
@@ -88,4 +88,7 @@ def forgot_password():
     return render_template("forgot_password.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
